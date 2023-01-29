7075b8432b30f497a9bce839a8e31b7772bc3ce0
Andy <andy.li5167@gmail.com>
2023-01-28 16:22:04 -0800

    Implemented the following changes:
    -performed code cleanup

--- src/main/java/org/example/WebpageMonitor.java
+++ src/main/java/org/example/WebpageMonitor.java
@@ -1,53 +1,59 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.io.IOException;
-
+import java.util.Arrays;
+import java.util.List;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.HttpResponse;
 import org.json.JSONObject;
 
 public class WebpageMonitor {
     public static void main(String[] args) throws IOException {
-        // URL of the webpage to monitor
-        ArrayList<String> urls = new ArrayList<String>();
-        urls.add("https://arcteryx.com/ca/en/shop/bird-head-toque");
-        urls.add("https://arcteryx.com/ca/en/shop/grotto-toque");
-        urls.add("https://arcteryx.com/ca/en/shop/bird-word-toque");
-        urls.add("https://arcteryx.com/ca/en/shop/rho-lightweight-wool-toque");
-
+        // URLs to monitor
+        List<String> urls = Arrays.asList(
+                "https://arcteryx.com/ca/en/shop/bird-head-toque",
+                "https://arcteryx.com/ca/en/shop/grotto-toque",
+                "https://arcteryx.com/ca/en/shop/bird-word-toque",
+                "https://arcteryx.com/ca/en/shop/rho-lightweight-wool-toque"
+        );
+        String webhook = "https://discord.com/api/webhooks/1068074137995706369/NJHQM0qaMp2iVY4EqmYZXhoCuLiH_mzbSvs3QIvv-JZ0_PJiCbXNMiK2yQHPq17WKZpg";
         // loop for each item in the urls list
         for (String url : urls) {
-            // Connect to the webpage
-            Document html = Jsoup.connect(url).get();
-            // Find the button element
-            Elements buttonElements = html.select("button");
-            boolean flag = false;
-            JSONObject payload = new JSONObject();
+            try {
+                // Connect to the webpage
+                Document html = Jsoup.connect(url).get();
+                // Find the button element
+                Elements buttonElements = html.select("button");
+                boolean flag = false;
+                JSONObject payload = new JSONObject();
 
-            for (Element button : buttonElements) {
-                if (button.text().equals("Add To Cart")) {
-                    flag = true;
-                    break;
+                for (Element button : buttonElements) {
+                    if (button.text().equals("Add To Cart")) {
+                        flag = true;
+                        break;
+                    }
                 }
-            }
-            if (flag) {
-                payload.put("content", "Add to cart button found for: " + url);
-                HttpResponse< JsonNode > jsonResponse = Unirest.post("https://discord.com/api/webhooks/1062994904382443570/rBDvD_8DVO_WyoUISZ85wsdqFjMe5_40cxwKq7_jBGp6Etu7CoZZKPLdHm1sI0qj43Lx")
+                if (flag) {
+                    payload.put("content", "Restocked: " + url);
+                }
+
+                HttpResponse<JsonNode> jsonResponse = Unirest.post(webhook)
                         .header("Content-Type", "application/json")
                         .body(payload)
                         .asJson();
-            } else {
-                payload.put("content", "Add to cart button not found for: " + url);
-                HttpResponse< JsonNode > jsonResponse = Unirest.post("https://discord.com/api/webhooks/1062994904382443570/rBDvD_8DVO_WyoUISZ85wsdqFjMe5_40cxwKq7_jBGp6Etu7CoZZKPLdHm1sI0qj43Lx")
-                        .header("Content-Type", "application/json")
-                        .body(payload)
-                        .asJson();
+                if (jsonResponse.getStatus() != 200) {
+                    System.out.println("Failed to send payloads to webhook. HTTP status code: " + jsonResponse.getStatus());
+                }
+            } catch (UnirestException e) {
+                System.out.println("Failed to send payloads to webhook. Error message: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Failed to connect to " + url + ". Error message: " + e.getMessage());
             }
         }
     }
